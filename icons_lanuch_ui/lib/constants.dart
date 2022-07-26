647433d79adff952751749aad48e97523f1f6eb8
4eug <debraheug@gmail.com>
2022-07-26 20:16:20 +0000

    constants

---
+++ icons_lanuch_ui/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+const Color kBlack = Color(0xFF000000);
+const Color kDarkGray = Color(0xFF181818);
+
+class UIGuide {
+// ignore: non_constant_identifier_names
+  static const String icons_3d = "assets/images/Airpod.png";
+}
