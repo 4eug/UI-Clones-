60a10422381cc9464bf0690224a0185c75157312
4eug <debraheug@gmail.com>
2022-07-26 21:15:01 +0000

    update

--- icons_lanuch_ui/lib/constants.dart
+++ icons_lanuch_ui/lib/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 const Color kBlack = Color(0xFF000000);
 const Color kDarkGray = Color(0xFF181818);
+const Color kWhite = Color(0xFFFFFFFF);
 
 class UIGuide {
 // ignore: non_constant_identifier_names
