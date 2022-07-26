82ff5e4c2d769b5f4c6fd029e0357d02c9eb645a
4eug <debraheug@gmail.com>
2022-07-26 20:37:20 +0000

    Image added

--- icons_lanuch_ui/lib/lanuch_screen.dart
+++ icons_lanuch_ui/lib/lanuch_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:icons_lanuch_ui/constants.dart';
 
 class LanuchScreen extends StatefulWidget {
   const LanuchScreen({Key? key}) : super(key: key);
@@ -10,10 +11,38 @@ class LanuchScreen extends StatefulWidget {
 class _LanuchScreenState extends State<LanuchScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('3d Lanuch Screen'),
-      ),
-    );
+    return Scaffold(
+        backgroundColor: kBlack,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 60, left: 40, right: 40),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    UIGuide.icons_3d,
+                    width: 380,
+                    height: 400,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
+}
+
+Widget _icons_3d() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      SizedBox(
+        width: 400,
+        child: Image.asset(UIGuide.icons_3d),
+      ),
+    ],
+  );
 }
